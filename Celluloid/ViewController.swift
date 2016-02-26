7dfbc9f8519ba8d311b39f8627e1d81352ab8b5f
MangoFang <100mango@gmail.com>
2016-02-26 15:31:53 +0800

    init

---
+++ Celluloid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Celluloid
+//
+//  Created by Mango on 16/2/26.
+//  Copyright © 2016年 Mango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
