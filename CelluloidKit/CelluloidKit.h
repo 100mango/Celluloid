e645a2f059287e403985f465aa3cfb432206a1f1
MangoFang <100mango@gmail.com>
2016-02-26 21:05:18 +0800

    init CelluloidKit

---
+++ CelluloidKit/CelluloidKit.h
@@ -0,0 +1,19 @@
+//
+//  CelluloidKit.h
+//  CelluloidKit
+//
+//  Created by Mango on 16/2/26.
+//  Copyright © 2016年 Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CelluloidKit.
+FOUNDATION_EXPORT double CelluloidKitVersionNumber;
+
+//! Project version string for CelluloidKit.
+FOUNDATION_EXPORT const unsigned char CelluloidKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CelluloidKit/PublicHeader.h>
+
+
