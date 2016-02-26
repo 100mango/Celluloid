aa8b7d943055d72abd87aa04f819e758715c6dae
MangoFang <100mango@gmail.com>
2016-02-26 22:29:52 +0800

    delete storyboard

--- Celluloid/AppDelegate.swift
+++ Celluloid/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.blackColor()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
         return true
     }
 
--- Celluloid/ViewController.swift
+++ Celluloid/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
